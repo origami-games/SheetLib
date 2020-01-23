5dd90607a0ae819c1763031aa10f7838efe18b0c
Daniel Orr <orrdaniel73@gmail.com>
2020-01-23 19:07:33 +0000

    v1.0.2
    + PistonImmovableBlock template class
    + Stripping functionality (will only work for blocks with an axis blockstate)
    + Loot table additions
    + Template item group
    * Update to 1.15.2
    * Corrected load log message

--- src/main/java/co/origamigames/sheet/SheetLib.java
+++ src/main/java/co/origamigames/sheet/SheetLib.java
@@ -2,18 +2,31 @@ package co.origamigames.sheet;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.tools.FabricToolTags;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.*;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SheetLib implements ModInitializer {
     public static final String MOD_ID = "sheet-lib";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        System.out.println("** SheetLib Loaded **");
+        LOGGER.info("[Sheet-Lib] Loaded");
     }
 
     // undefined block
@@ -31,6 +44,30 @@ public class SheetLib implements ModInitializer {
 
         return block;
     }
+    // add stripping functionality for a pair of blocks (ONLY WORKS FOR BLOCKS WITH AN AXIS BLOCKSTATE)
+    public static void addStrippingFunctionality(Block blockToBeStripped, Block blockAfterStrip) {
+        UseBlockCallback.EVENT.register((player, world, hand, hit) -> {
+            if (player.getStackInHand(hand).getItem().isIn(FabricToolTags.AXES) && world.getBlockState(hit.getBlockPos()).getBlock() == blockToBeStripped) {
+                BlockPos blockPos = hit.getBlockPos();
+                BlockState blockState = world.getBlockState(blockPos);
+
+                world.playSound(player, blockPos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                if (!world.isClient) {
+                    world.setBlockState(blockPos, (BlockState)blockAfterStrip.getDefaultState().with(PillarBlock.AXIS, blockState.get(PillarBlock.AXIS)), 11);
+                    if (!player.isCreative()) {
+                        ItemStack stack = player.getStackInHand(hand);
+                        stack.damage(1, player, ((p) -> {
+                            p.sendToolBreakStatus(hand);
+                        }));
+                    }
+                }
+
+                return ActionResult.SUCCESS;
+            }
+
+            return ActionResult.PASS;
+        });
+    }
 
     // undefined item
     public static Item item(String mod_id, String id, Item item) {
@@ -43,9 +80,26 @@ public class SheetLib implements ModInitializer {
     // spawn egg item
     @SuppressWarnings("rawtypes")
     public static Item spawnEggItem(String mod_id, String entity_id, ItemGroup item_group, int max_count, int maxCount, EntityType entity, int primaryColor,
-                             int secondaryColor) {
+                                    int secondaryColor) {
         return Registry.register(Registry.ITEM, new Identifier(mod_id, entity_id + "_spawn_egg"),
                 new SpawnEggItem(entity, primaryColor, secondaryColor,
                         new Item.Settings().maxCount(max_count).group(item_group)));
+    }
+
+    // loot table addition
+    public static void lootTableAddition(String mod_id, String loot_table) {
+        Identifier VANILLA_TABLE = new Identifier("minecraft", loot_table);
+        Identifier ADDITION_TABLE = new Identifier(mod_id, "additions/" + loot_table);
+
+        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+            if (VANILLA_TABLE.equals(id)) {
+                supplier.copyFrom(lootManager.getSupplier(ADDITION_TABLE));
+            }
+        });
+    }
+
+    // item group template
+    public static ItemGroup itemGroup(String mod_id, String id, ItemStack item_stack) {
+        return FabricItemGroupBuilder.build(new Identifier(mod_id, "item_group"), () -> item_stack);
     }
 }
---
+++ src/main/java/co/origamigames/sheet/block/PistonImmovableBlock.java
@@ -0,0 +1,16 @@
+package co.origamigames.sheet.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.piston.PistonBehavior;
+
+public class PistonImmovableBlock extends Block {
+    public PistonImmovableBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public PistonBehavior getPistonBehavior(BlockState blockState) {
+        return PistonBehavior.BLOCK;
+    }
+}
