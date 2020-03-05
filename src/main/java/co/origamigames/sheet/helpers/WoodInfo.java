6a28a3623038086697d12c5200cfa62aae4eefb4
Daniel Orr <orrdaniel73@gmail.com>
2020-03-05 08:14:19 +0000

    Fix WoodInfo being an abstract class
    Couldn't be instantiated

--- src/main/java/co/origamigames/sheet/helpers/WoodInfo.java
+++ src/main/java/co/origamigames/sheet/helpers/WoodInfo.java
@@ -2,7 +2,7 @@ package co.origamigames.sheet.helpers;
 
 import net.minecraft.block.MaterialColor;
 
-public abstract class WoodInfo {
+public class WoodInfo {
     public String name;
     public boolean hasLeaves = true;
     public MaterialColor barkColor;
